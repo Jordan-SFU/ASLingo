825de8a56fc93bbfd5bdc2b1bba0d55fb070675e
Nicholas Chan <nicholasch.an@outlook.com>
2023-11-19 13:29:34 -0800

    yay

--- app.py
+++ app.py
@@ -4,21 +4,34 @@ from flask_login import UserMixin
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisissecret'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/learning')
 def learning():
     return render_template('learning.html')
 
-@app.route('/login')
-def login():
-    return render_template('homePage.html')
-
 @app.route('/translator')
 def translator():
     return render_template('translator.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
